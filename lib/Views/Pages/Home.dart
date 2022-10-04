81bd97d3d556cc11f2cf3ec2fda2322f0d0cc905
sakibovi123 <sakibovi123@gmail.com>
2022-10-05 00:15:04 +0600

    news card added

--- lib/Views/Pages/Home.dart
+++ lib/Views/Pages/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wrestling_news_app/Views/Widgets/NewsCard.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -15,33 +16,63 @@ class _HomeState extends State<Home> {
         elevation: 0,
         backgroundColor: Colors.white,
         title: const Text(
-          "WWE",
+          "Wrestling",
           style: TextStyle(
             fontStyle: FontStyle.italic,
             fontSize: 18.0,
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            color: Colors.white24,
-            child: Column(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                // ignore: prefer_const_constructors
-                Text("Trending News",
-                    style: const TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.bold,
-                    )),
-              ],
-            ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.favorite_border_outlined)
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              // height: double.infinity,
+              color: Colors.white24,
+              child: Column(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  // ignore: prefer_const_constructors
+                  Text("Trending News",
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: NewsCard(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: NewsCard(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: NewsCard(),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: NewsCard(),
+                  ),
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const BottomAppBar(),
     );
   }
 }
--- lib/Views/Widgets/NewsCard.dart
+++ lib/Views/Widgets/NewsCard.dart
@@ -1,12 +1,80 @@
 import 'package:flutter/material.dart';
 
-
-
 class NewsCard extends StatelessWidget {
   const NewsCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Container(
+        color: const Color(0xFFf5f5f5),
+        height: 350.0,
+        padding: const EdgeInsets.all(10.0),
+        width: double.infinity,
+        
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/digital2.png",
+              height: 150.0,
+            ),
+            const Text("Lorem Ipsum Dolor Lorem lorem ipsum",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            const Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                  "Lorem ipsum dolor lorem ipsum dolor dolor ipsum lorem ipsum dolor dolor ipsum lorem, lorem ipsum dolor dolor dolor dolor dolor dolor dolor... read more..."),
+            ),
+            Container(
+              height: 15.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Author: Name",
+                  style: TextStyle(
+                    fontSize: 11.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Posted: 10/22/2052",
+                  style: TextStyle(
+                    fontSize: 11.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text("Vote: 10",
+                  style: TextStyle(
+                    fontSize: 11.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+              ],
+            ),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
