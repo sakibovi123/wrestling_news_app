055227b24b79c78e3824f6b096bcbd6ab240bb6e
FaizAhamed2o1 <faizahmed23014@gmail.com>
2022-11-10 00:56:00 +0600

    removed pageview builder from result details

--- lib/Views/Widgets/result_details.dart
+++ lib/Views/Widgets/result_details.dart
@@ -22,41 +22,15 @@ class _ResultDetailsState extends State<ResultDetails> {
     return Scaffold(
       body: Stack(
         children: [
-          PageView.builder(
-            itemCount: 5,
-            scrollDirection: Axis.vertical,
+          SingleChildScrollView(
             physics: ClampingScrollPhysics(),
-            controller: pageController,
-            itemBuilder: (context, index) {
-              return ShowResultCard(
-                width: width,
-                height: height,
-                resultTitle: 'WWE Raw Results, Oct 31, 2022',
-                resultDescription:
-                    'Women\'s Tag Team Championship: Alexa Bliss & Asuka def. Damage CTRL (Iyo Sky & Dakota Kai) (c) - TITLE CHANGE!!!',
-              );
-            },
-          ),
-          Positioned(
-            bottom: 10,
-            right: 10,
-            child: Container(
-              width: 35,
-              margin: EdgeInsets.only(right: 10, bottom: 20),
-              child: FloatingActionButton(
-                onPressed: () {
-                  setState(() {});
-                  pageController.animateToPage(
-                    _initialPage,
-                    curve: Curves.decelerate,
-                    duration: Duration(milliseconds: 500),
-                  );
-                },
-                child: Icon(
-                  Icons.arrow_upward_outlined,
-                  size: 18,
-                ),
-              ),
+            scrollDirection: Axis.vertical,
+            child: ShowResultCard(
+              width: width,
+              height: height,
+              resultTitle: 'WWE Raw Results, Oct 31, 2022',
+              resultDescription:
+                  'Women\'s Tag Team Championship: Alexa Bliss & Asuka def. Damage CTRL (Iyo Sky & Dakota Kai) (c) - TITLE CHANGE!!!',
             ),
           ),
           CustomAppbar(),
